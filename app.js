233e72a75d52e696ac450f47351cf712afc02e93
Marc Cuesta <marc.cuestacarabasa@gmail.com>
2022-06-19 11:05:07 +0200

    Solved Lab

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ app.get('/', (req, res, next) => {
     res.sendFile(`${__dirname}/views/index.html`);
 })
 
+app.get('/about', (req, res, next) => {
+    res.sendFile(`${__dirname}/views/about.html`);
+})
+
+app.get('/works', (req, res, next) => {
+    res.sendFile(`${__dirname}/views/works.html`);
+})
+
+app.get('/gallery', (req, res, next) => {
+    res.sendFile(`${__dirname}/views/gallery.html`)
+})
+
 
 const port = 3000;
 app.listen(port, () => console.log(`app listen at port ${3000}`));
